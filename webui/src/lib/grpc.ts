2b658cfdaf3463db7b86a4b308d26fdaeb65a0e1
Daniel 'f0o' Preussker <f0o@devilcode.org>
2024-09-03 06:37:22 +0000

    Implement Blob Downloads for output >10MB Implement prettier for JS/Svelte formatting (so pretty now...)

--- webui/src/lib/grpc.ts
+++ webui/src/lib/grpc.ts
@@ -1,17 +1,20 @@
-import { LookingGlassService } from '@as203038/lg-protobuf/lookingglass/v0/lookingglass_connect';
-export type * as Pb from '@as203038/lg-protobuf/lookingglass/v0/lookingglass_pb';
+import { LookingGlassService } from "@as203038/lg-protobuf/lookingglass/v0/lookingglass_connect";
+export type * as Pb from "@as203038/lg-protobuf/lookingglass/v0/lookingglass_pb";
 import { createGrpcWebTransport } from "@connectrpc/connect-web";
 import { createPromiseClient, type PromiseClient } from "@connectrpc/connect";
-import { env } from '$env/dynamic/public';
+import { env } from "$env/dynamic/public";
 
 let client: PromiseClient<typeof LookingGlassService> | null = null;
 
 export const LookingGlassClient = () => {
-    if (!client) {
-        client = createPromiseClient<typeof LookingGlassService>(LookingGlassService, createGrpcWebTransport({
-            baseUrl: env.PUBLIC_GRPC_URL,
-            useBinaryFormat: true
-        }) as any); // Add 'as any' to bypass type checking
-    }
-    return client;
-}
+  if (!client) {
+    client = createPromiseClient<typeof LookingGlassService>(
+      LookingGlassService,
+      createGrpcWebTransport({
+        baseUrl: env.PUBLIC_GRPC_URL,
+        useBinaryFormat: true,
+      }) as any,
+    ); // Add 'as any' to bypass type checking
+  }
+  return client;
+};
